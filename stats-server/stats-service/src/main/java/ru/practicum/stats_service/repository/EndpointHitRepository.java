0c94b6b5f8a844d20cf1ee3a6067a0a502cca9bb
AnastasiyaK <stasykorchagina@yandex.ru>
2023-02-21 23:03:34 +0500

    "feat: Реализовать сервис статистики"

--- stats-server/stats-service/src/main/java/ru/practicum/stats_service/repository/EndpointHitRepository.java
+++ stats-server/stats-service/src/main/java/ru/practicum/stats_service/repository/EndpointHitRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 
 @Repository
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Integer> {
-    @Query("SELECT new ru.practicum.stats_dto.ViewStats(e.app, e.uri, count(e.ip))" +
+    @Query(value = "SELECT new ru.practicum.stats_dto.ViewStats(e.app, e.uri, count(e.ip))" +
             "from EndpointHit as e where e.timestamp between ?1 and ?2 and e.uri in (?3) group by e.app, e.uri " +
             "order by count (e.ip)DESC ")
     List<ViewStats> countHit(LocalDateTime start, LocalDateTime end, List<String> uri);
 
-    @Query("SELECT new ru.practicum.stats_dto.ViewStats(" +
+    @Query(value = "SELECT new ru.practicum.stats_dto.ViewStats(" +
             "e.app, e.uri, count(distinct e.ip))" +
             "from EndpointHit as e where e.timestamp between ?1 and ?2 and e.uri in (?3) group by e.app, e.uri " +
             "order by count (e.ip)DESC ")
--- stats-server/stats-service/src/main/resources/sсhema.sql
+++ stats-server/stats-service/src/main/resources/sсhema.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS endpoint_hit;
-CREATE TABLE IF NOT EXISTS endpoint_hit
+DROP TABLE IF EXISTS hits;
+CREATE TABLE IF NOT EXISTS hits
 (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     app VARCHAR,
+    apps VARCHAR,
     uri VARCHAR,
     ip VARCHAR,
     timestamp timestamp with time zone
